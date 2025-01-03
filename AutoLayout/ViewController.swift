dbb59c5098f4aec4a3689604428df099f5f92b45
smhmurat <smhmurat@gmail.com>
2025-01-03 12:08:55 +0300

    Initial Commit

---
+++ AutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Semih Murat on 3.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
